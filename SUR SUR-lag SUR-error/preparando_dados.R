8c71aee73b4e1936e00560339dfc38a5c60361d5
rdurl0 <durlo.raul@gmail.com>
2017-07-23 23:00:15 -0300

    tbelas de ocorrencias por natureza

--- SUR SUR-lag SUR-error/preparando_dados.R
+++ SUR SUR-lag SUR-error/preparando_dados.R
@@ -9,16 +9,20 @@ library(readxl)
 #'
 #################################
 
+# Diretório
 setwd("C:\\Users\\Raul\\Documents\\meu_projeto\\SUR SUR-lag SUR-error")
 dir()
 
+# leitura do arquivi
 ocorr_natureza <- read_excel("tidy_agredados_ssp.xlsx", sheet="Plan1", na="-")
 
+# guarda os nomes para tabela de descrição
 xlsx <- tibble(Cod = seq(1:11),
                XLSX = names(ocorr_natureza[,1:11]))
 xlsx
 
-ocorr_natureza$período <- ocorr_natureza$`Ocorrência/período`
+# variáveis com novos nomes
+ocorr_natureza$ano <- ocorr_natureza$`Ocorrência/período`
 ocorr_natureza$pessoa <- ocorr_natureza$`Contra a pessoa`
 ocorr_natureza$patrimônio <- ocorr_natureza$`Contra o patrimônio`
 ocorr_natureza$contravencionais <- ocorr_natureza$`Contravencio-is`
@@ -27,7 +31,8 @@ ocorr_natureza$outros_delitos <- ocorr_natureza$`Outros delitos (Inclui contrave
 ocorr_natureza$violentos <- ocorr_natureza$`Total de Crimes Violentos ( Hom.Doloso, Roubo, Latrocínio, Estupro e EMS)`
 ocorr_natureza$total_delitos <- ocorr_natureza$`Total de delitos`
 ocorr_natureza$costumes <- ocorr_natureza$`Contra os constumes (*)`
-  
+ocorr_natureza$entorpecentes <- ocorr_natureza$Entorpecentes
+  # exclui as variáveis com nome tidy
 ocorr_natureza$`Ocorrência/período` <- NULL
 ocorr_natureza$`Contra a pessoa` <- NULL
 ocorr_natureza$`Contra o patrimônio`  <- NULL
@@ -38,30 +43,57 @@ ocorr_natureza$`Total de Crimes Violentos ( Hom.Doloso, Roubo, Latrocínio, Estu
 ocorr_natureza$`Total de delitos` <- NULL
 ocorr_natureza$`Contra os constumes (*)` <- NULL
 ocorr_natureza$grupo <- NULL
+ocorr_natureza$Entorpecentes <- NULL
 
-ocorr_natureza <- ocorr_natureza %>% select("período", "local", "pessoa", "patrimônio",
-                                            "costumes", "Entorpecentes", "contravencionais", "outros_criminais",
+# reordena para mesclar com a tabela de descrição
+ocorr_natureza <- ocorr_natureza %>% select("ano", "local", "pessoa", "patrimônio",
+                                            "costumes", "entorpecentes", "contravencionais", "outros_criminais",
                                             "outros_delitos", "violentos", "total_delitos")
-names(ocorr_natureza)
-xlsx
 
+# cria tabela de descrição
 descricao_natureza <- tibble(Cod = xlsx$Cod,
                              XLSX = xlsx$XLSX,
                              Variável = names(ocorr_natureza),
-                             Descrição = c("período",
-                                           "interior, Gde Sp, Capital",
+                             Descrição = c("Ano de registro das ocorrências",
+                                           "Interior, Grande São Paulo, Capital",
                                            "Ocorrências de crime contra pessoa",
                                            "Ocorrências de crime contra o patrimònio",
                                            "Ocorrências de crime contra os costumes (até 2009)/contra a dignidade sexual (2010-atual)",
-                                           "Ocorrências de Tráfico de Entorpecentes",
+                                           "Ocorrências de tráfico de Entorpecentes",
                                            "Ocorrências de contravenções (https://goo.gl/QccSm2)",
                                            "Ocorrências de outros criminais - exceto contravenções",
-                                           "Ocorências de Outros delitos - inclusive contravenções",
+                                           "Ocorências de outros delitos - inclusive contravenções",
                                            "Total de crimes violentos (Homicidio Doloso, Roubo, Latrocínio, Estupro e EMS)",
                                            "Total de delitos"))
 
+# separa ano de trimestre
+trim <- rep(c(rep(1, 3), rep(2, 3), rep(3, 3), rep(4, 3)), 20)
+ocorr_natureza$trimestre <- c(rep(3,3), rep(4,3), trim, rep(1,3))
+
+# precisou reordenar de novo :(
+ocorr_natureza <- ocorr_natureza %>% select("ano", "trimestre", "local", "pessoa", "patrimônio",
+                                            "costumes", "entorpecentes", "contravencionais", "outros_criminais",
+                                            "outros_delitos", "violentos", "total_delitos")
+
+# e tira os caracteres indesejados
+ocorr_natureza$local <- gsub("Gde", "Grande", ocorr_natureza$local)
+ocorr_natureza$ano <- gsub("-1T", "", ocorr_natureza$ano)
+ocorr_natureza$ano <- gsub("-2T", "", ocorr_natureza$ano)
+ocorr_natureza$ano <- gsub("-3T", "", ocorr_natureza$ano)
+ocorr_natureza$ano <- gsub("-4T", "", ocorr_natureza$ano)
+ocorr_natureza$ano <- gsub("-T4", "", ocorr_natureza$ano)
+ocorr_natureza$ano <- as.integer(ocorr_natureza$ano)
+
+# Mais uma vez atualiza a tabela de descrição
+descricao_natureza <- rbind(descricao_natureza[1,],
+                      c("", "Ocorrência/período", "trimestre", "Trimestre de registro das ocorrências"),
+                      descricao_natureza[2:11,])
+descricao_natureza$Cod <- seq(1:12)
 View(descricao_natureza)
-View(ocorr_natureza)
+
+# Agora salve e faça bom uso no report...
+write_rds(descricao_natureza, "C:\\Users\\Raul\\Documents\\meu_projeto\\SUR SUR-lag SUR-error\\serie_trimestral_descricao_ocorrencias_por_natureza.rds")
+write_rds(ocorr_natureza, "C:\\Users\\Raul\\Documents\\meu_projeto\\SUR SUR-lag SUR-error\\serie_trimestral_ocorrencias_por_natureza.rds")
 
 
 #################################
